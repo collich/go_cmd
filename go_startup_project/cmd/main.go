02be7d4868608854219d883021e6d5a48c69afe9
aadmin <collin-chua@hotmail.com>
2024-03-24 15:26:18 +0800

    Added feature to read directory

--- go_startup_project/cmd/main.go
+++ go_startup_project/cmd/main.go
@@ -9,23 +9,37 @@ import (
 
 
 func main() {
+	// Hard 
 	files_add := []string{"bin", "cmd", "internal", "tests"}
 	file_perm := 751
 
+	// Get current working directory
 	filepath, err := os.Getwd()
 	if err != nil{
 		log.Fatal(err)
 	}
 
+	// Add Directory after getting current directory
 	for _, file := range files_add{
-		total_path := filepath + file
+		total_path := filepath + "/" +file
 		err := os.Mkdir(total_path, fs.FileMode(file_perm))
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Added file %s in %s with permission %s\n", file, filepath, fs.FileMode(file_perm).String())
+		fmt.Printf("Added %s folder.\n", file)
 	}
 
-	
-	// fmt.Println(filepath)
+	// Read Directory after reading
+	files, err := os.ReadDir(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("File Present in ", filepath, ":")
+	for _, file := range files{
+		if file.Type().IsDir(){
+			fmt.Println(file.Name())
+		}
+	}
+
 }
