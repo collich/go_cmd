e25e65e723f52dbc326c7d00823a6ab7b73f8874
aadmin <collin-chua@hotmail.com>
2024-03-24 14:55:44 +0800

    Added part that add directory in a loop

--- go_startup_project/cmd/main.go
+++ go_startup_project/cmd/main.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
 )
 
 
 func main() {
-	// 751
-	// files_add := []string{"bin", "cmd", "internal", "tests"}
-	// file_perm := 751
+	files_add := []string{"bin", "cmd", "internal", "tests"}
+	file_perm := 751
 
 	filepath, err := os.Getwd()
 	if err != nil{
 		log.Fatal(err)
 	}
 
-	fmt.Println(filepath)
+	for _, file := range files_add{
+		total_path := filepath + file
+		err := os.Mkdir(total_path, fs.FileMode(file_perm))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Added file %s in %s with permission %s\n", file, filepath, fs.FileMode(file_perm).String())
+	}
+
+	
+	// fmt.Println(filepath)
 }
