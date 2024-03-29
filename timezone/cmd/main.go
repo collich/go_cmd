a5a4c3e46658dba97d17842ef5a949c484871db5
aadmin <collin-chua@hotmail.com>
2024-03-29 17:52:54 +0800

    Added a condition for status code

--- timezone/cmd/main.go
+++ timezone/cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	// "fmt"
+	"log"
+	"net/http"
+)
+
 func main()  {
+	res, err := http.Get("http://worldtimeapi.org/api/timezone")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
 	
+	if res.StatusCode != 200{
+		log.Panicf("Got Status code: %d", res.StatusCode)
+	}
+
 }
